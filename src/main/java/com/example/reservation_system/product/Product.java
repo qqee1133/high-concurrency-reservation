20c7e0643d96a60c88baf4dfc9e59c60c2e0ba55
user <dddsa1313@gmail.com>
2025-11-16 04:45:36 +0900

    feat(domain): Product 엔티티 및 리포지토리 구현

---
+++ src/main/java/com/example/reservation_system/product/Product.java
@@ -0,0 +1,35 @@
+package com.example.reservation_system.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    private static final long MIN_STOCK = 0L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private long stock;
+
+    public Product(String name, long stock) {
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public void decreaseStock() {
+        if (this.stock <= MIN_STOCK) {
+            throw new RuntimeException("재고가 소진되었습니다.");
+        }
+        this.stock -= 1;
+    }
+}
